d350fb51fb40c207673de61d2095aaaeb533494c
dangdaizheshi <2022702538@qq.com>
2025-11-07 15:36:20 +0800

    改善评论列表功能

--- src/App.js
+++ src/App.js
@@ -1,145 +1,122 @@
-import { useRef, useState } from 'react'
-import './index.scss'
-
+import { useRef, useState } from 'react';
+import './index.scss';
 const defaultList = [
   {
-    rpid: 3,
-    user: {
-      uid: '13258165',
-      avatar: '',
-      uname: '周杰伦',
-    },
-    content: '哎哟，不错哦',
-    ctime: '10-18 08:15',
-    like: 89,
+    id: new Date(),
+    name: '漳卅',
+    content: '我是法外狂徒'
   },
   {
-    rpid: 2,
-    user: {
-      uid: '36080105',
-      avatar: '',
-      uname: '许嵩',
-    },
-    content: '我寻你千百度 日出到迟暮',
-    ctime: '11-13 11:29',
-    like: 88,
+    id: new Date(),
+    name: '呃呃呃',
+    content: '范德萨'
   },
   {
-    rpid: 1,
-    user: {
-      uid: '30009257',
-      avata: '',
-      uname: '前端',
-    },
-    content: '学前端就',
-    ctime: '10-19 09:00',
-    like: 66,
-  },
-]
-// 当前登录用户信息
-const user = {
-  uid: '30009257',
-  avatar: '',
-  uname: '黑马前端',
-}
-
-const tabs = [
-  { type: 'like', text: '最热' },
-  { type: 'ctime', text: '最新' },
-]
-const App = () => {
-  const [commentList, setcommentList] = useState(defaultList);
-  const [type, settype] = useState('like');
-  const [content, setcontent] = useState('');
-
-  const deleteComment = (id) => {
-    setcommentList(commentList.filter(item => item.user.uid !== id));
+    id: new Date(),
+    name: 'fgf',
+    content: '的广告'
   }
+]
+function App() {
+  let [list, setlist] = useState(defaultList);
+  let [iptName, setiptName] = useState('');
+  let [iptContent, setiptContent] = useState('');
+  const ipt = useRef('');
+  let [toptag, settoptag] = useState('fresh');
+  const a = [
+    {
+      tag: 'fresh',
+      content: '最新'
+    },
+    {
+      tag: 'hot',
+      content: '最热'
+    }
+  ]
 
-  const handleTab = (id) => {
-    settype(id);
-    if(id === 'like') {
-
+  // 添加
+  const handleAdd = () => {
+    if(iptName === '') {
+      alert('请输入名字');
+      return;
+    }
+    else if(iptContent === '') {
+      alert('评论');
+      return;
+    }
+    setlist([
+      {
+        id: new Date(),
+        name: iptName,
+        content: iptContent
+      },
+      ...list
+    ])
+    setiptName('');
+    setiptContent('');
+    ipt.current.focus();
+  }
+  // 删除
+  const handleDel = (id) => {
+    setlist(list.filter(item => (item.id !== id)));
+  }
+  // tab切换
+  const changeTag = (id) => {
+    settoptag(id);
+    if(id === 'fresh') {
+      setlist(list.sort((a, b) => {
+        return a.name - b.name;
+      }))
+    }
+    else {
+      setlist(list.sort((a, b) => {
+        return a.content - b.content;
+      }))
     }
   }
 
-  return (
-    <div className="app">
-      {/* 导航 Tab */}
-      <div className="reply-navigation">
-        <ul className="nav-bar">
-          <li className="nav-title">
-            <span className="nav-title-text">评论</span>
-            {/* 评论数量 */}
-            <span className="total-reply">{10}</span>
-          </li>
-          <li className="nav-sort">
-            {/* 高亮类名： active */}
-              {tabs.map(item => (
-                <span 
-                  className={`nav-item ${type === item.type && 'active'}`}
-                  key = {item.type} 
-                  onClick={() => handleTab(item.type)}>
-                  {item.text}
-                </span>
-              )
-              )}
-          </li>
-        </ul>
-      </div>
 
-      <div className="reply-wrap">
-        {/* 发表评论 */}
-        <div className="box-normal">
-          {/* 当前用户头像 */}
-          <div className="reply-box-avatar">
-            <div className="bili-avatar">
-              <img className="bili-avatar-img" alt="用户头像" />
-            </div>
-          </div>
-          <div className="reply-box-wrap">
-            {/* 评论框 */}
-            <textarea
-              className="reply-box-textarea"
-              placeholder="发一条友善的评论"
-              value = {content}
-              onChange={(e) => setcontent(e.target.value)}
-            />
-            {/* 发布按钮 */}
-            <div className="reply-box-send">
-              <div className="send-text">发布</div>
-            </div>
-          </div>
-        </div>
-        {/* 评论列表 */}
-        <div className="reply-list">
-          {/* 评论项 */}
-          {commentList.map(item => (
-            <div className="reply-item">
-              <div className="root-reply-avatar">
-                <div className="bili-avatar">
-                  <img className="bili-avatar-img"/>
-                </div>
-              </div>
-              <div className="content-wrap">
-                <div className="user-info">
-                  <div className="user-name">{item.user.uname}</div>
-                </div>
-                <div className="root-reply">
-                  <span className="reply-content">{item.content}</span>
-                  <div className="reply-info">
-                    <span className="reply-time">{item.ctime}</span>
-                    <span className="reply-time">点赞数:{item.like}</span>
-                    {item.user.uid === user.uid && <span className='delete' onClick={() => deleteComment(user.uid)}>删除</span>}
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
+  // main
+  return (
+    <div className="App">
+      <div className='banner'>
+        {a.map(item => (
+          <span 
+            key = {item.tag} 
+            onClick={() => changeTag(item.tag)}
+            className={`${toptag === item.tag && 'active'}`}>
+            {item.content}
+          </span>
+        ))}
       </div>
+      <div className="top">
+        <input 
+          value={iptName}
+          onChange={(e) => setiptName(e.target.value)}
+          type="text"
+          ref = {ipt}
+          placeholder="请输入名字"/>
+        <button onClick={handleAdd}>发布</button>
+        <br />
+        <textarea 
+          value={iptContent}
+          onChange={(e) => setiptContent(e.target.value)}
+          className = "kuang" 
+          placeholder="请输入"/>
+      </div>
+      {!list.length ? (<div className="non">暂无评论</div>) : (
+        <ul>
+        {list.map(item => (
+          <li key = {item.id} className='lli'>
+            <span>{item.name}</span>
+            <span>{item.content}</span>
+            <span onClick={() => handleDel(item.id)}>删除</span>
+          </li>
+        ))}
+      </ul>
+      )}
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ 评论列表/01/App.js
@@ -0,0 +1,168 @@
+import { useRef, useState } from 'react'
+import './index.scss'
+
+const defaultList = [
+  {
+    rpid: 3,
+    user: {
+      uid: '13258165',
+      avatar: '',
+      uname: '周杰伦',
+    },
+    content: '哎哟，不错哦',
+    ctime: '10-18 08:15',
+    like: 89,
+  },
+  {
+    rpid: 2,
+    user: {
+      uid: '36080105',
+      avatar: '',
+      uname: '许嵩',
+    },
+    content: '我寻你千百度 日出到迟暮',
+    ctime: '11-13 11:29',
+    like: 88,
+  },
+  {
+    rpid: 1,
+    user: {
+      uid: '30009257',
+      avata: '',
+      uname: '前端',
+    },
+    content: '学前端就',
+    ctime: '10-19 09:00',
+    like: 66,
+  },
+]
+// 当前登录用户信息
+const user = {
+  uid: '30009257',
+  avatar: '',
+  uname: '黑马前端',
+}
+
+const tabs = [
+  { type: 'like', text: '最热' },
+  { type: 'ctime', text: '最新' },
+]
+const App = () => {
+  let pushRef = useRef(null);
+  let [commentList, setcommentList] = useState(defaultList);
+  let [type, settype] = useState('like');
+  let [content, setcontent] = useState('');
+
+  const deleteComment = (id) => {
+    setcommentList(commentList.filter(item => (item.rpid !== id)));
+  }
+
+  const handleTab = (id) => {
+    settype(id);
+    if(id === 'like') {
+      setcommentList(commentList.sort((a, b) => {
+        return a.like - b.like;
+      }))
+    }
+  }
+
+  const handlePush = () => {
+    const tim = new Date();
+    setcommentList([
+      ...commentList,
+      {
+        rpid: new Date(),
+        user: {
+          uid: '30009257',
+          avata: '',
+          uname: '张三',
+        },
+        content: content,
+        ctime: `${tim.getMonth() + 1}-${tim.getDate()} ${tim.getHours()}:${tim.getMinutes()}`,
+        like: 66,
+      }
+    ])
+    setcontent('');
+    pushRef.current.focus();
+  }
+  return (
+    <div className="app">
+      {/* 导航 Tab */}
+      <div className="reply-navigation">
+        <ul className="nav-bar">
+          <li className="nav-title">
+            <span className="nav-title-text">评论</span>
+            {/* 评论数量 */}
+            <span className="total-reply">{10}</span>
+          </li>
+          <li className="nav-sort">
+            {/* 高亮类名： active */}
+              {tabs.map(item => (
+                <span 
+                  className={`nav-item ${type === item.type && 'active'}`}
+                  key = {item.type} 
+                  onClick={() => handleTab(item.type)}>
+                  {item.text}
+                </span>
+              )
+              )}
+          </li>
+        </ul>
+      </div>
+
+      <div className="reply-wrap">
+        {/* 发表评论 */}
+        <div className="box-normal">
+          {/* 当前用户头像 */}
+          <div className="reply-box-avatar">
+            <div className="bili-avatar">
+              <img className="bili-avatar-img" alt="用户头像" />
+            </div>
+          </div>
+          <div className="reply-box-wrap">
+            {/* 评论框 */}
+            <textarea
+              className="reply-box-textarea"
+              placeholder="发一条友善的评论"
+              ref = {pushRef}
+              value = {content}
+              onChange={(e) => setcontent(e.target.value)}
+            />
+            {/* 发布按钮 */}
+            <div className="reply-box-send">
+              <div className="send-text" onClick={handlePush}>发布</div>
+            </div>
+          </div>
+        </div>
+        {/* 评论列表 */}
+        <div className="reply-list">
+          {/* 评论项 */}
+          {commentList.map(item => (
+            <div className="reply-item">
+              <div className="root-reply-avatar">
+                <div className="bili-avatar">
+                  <img className="bili-avatar-img"/>
+                </div>
+              </div>
+              <div className="content-wrap">
+                <div className="user-info">
+                  <div className="user-name">{item.user.uname}</div>
+                </div>
+                <div className="root-reply">
+                  <span className="reply-content">{item.content}</span>
+                  <div className="reply-info">
+                    <span className="reply-time">{item.ctime}</span>
+                    <span className="reply-time">点赞数:{item.like}</span>
+                    {item.user.uid === user.uid && <span className='delete' onClick={() => deleteComment(item.rpid)}>删除</span>}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
